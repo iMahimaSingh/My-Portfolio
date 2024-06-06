682eb2dbcdf99cb9507a03aa08de4d61d96991f9
iMahimaSingh <155384344+iMahimaSingh@users.noreply.github.com>
2024-06-06 18:20:12 +0530

    added javascript

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     var tablinks = document.getElementsByClassName("tab-links");
     var tabcontents = document.getElementsByClassName("tab-contents");
-  
-    function opentab(event, tabname) {
-      for (let tablink of tablinks) {
-        tablink.classList.remove("active-link");
-      }
-      for (let tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-      }
-      event.currentTarget.classList.add("active-link");
-      document.getElementById(tabname).classList.add("active-tab");
+
+    function opentab(tabname, event) {
+        for (let tablink of tablinks) {
+            tablink.classList.remove("active-link");
+        }
+        for (let tabcontent of tabcontents) {
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
     }
-  
-    // Optionally, attach the function to the buttons programmatically
+
     for (let tablink of tablinks) {
-      tablink.addEventListener("click", function(event) {
-        const tabname = this.getAttribute("data-tab"); // Assuming you have data-tab attributes
-        opentab(event, tabname);
-      });
+        tablink.addEventListener("click", function(event) {
+            const tabname = this.getAttribute("data-tab");
+            opentab(tabname, event);
+        });
     }
-  });
-  
+});
